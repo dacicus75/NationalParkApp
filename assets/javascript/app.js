b6accaf8a8e87227660d720baf3482bfd4c3df38
Chris Paul <cpaul319@users.noreply.github.com>
2019-03-09 13:23:15 -0500

    inital ideas on content

---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+
+
+//  --------------------------------------------------------------------------------
+  
+
+
+
+//create content with dynamic divs
+//app components
+
+//main page 
+// Articles – NPS API (homepage limit 3 articles)
+// dropdown with the following
+// Find campsite/lodging/visitor center - NPS API
+// button to initiate the search for National Park services
+// Search for nearest parks - NPS API (limit 3)
+
+
+
+//individual campsite
+// Directions -google maps api
+
+
+//individual lodging
+// Directions -google maps api
+
+
+
+//individual visitor centers
+// Directions -google maps api
+
+
+
+//parks list page
+//     Weather alerts -NPS API banner if an alert
+
+
+
+//indiviual park information
+// Directions -google maps api
+//  --------------------------------------------------------------------------------
+  

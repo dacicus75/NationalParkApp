2ee3d5a7c9a83ff7b03c10bdf1b637a5524feb96
Chris Paul <cpaul319@users.noreply.github.com>
2019-03-09 14:05:35 -0500

    changes to javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,9 +32,46 @@
 
 
 //parks list page
-//     Weather alerts -NPS API banner if an alert
+ 
 
-
+    function displayParks() {
+        $("#displayContent").on("click",  function () {
+            
+            var park = $(this).attr("data-park");
+            // example query
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                park + "&api_key=0s3GhZ60yFQgSvoDZ5rfXYPJB3urUXBN&limit=10&offset=0&rating=PG&lang=en";
+               
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+                .then(function (response) {
+                    console.log(queryURL);
+                    console.log(response);
+                    var results = response.data;
+                    for (var i = 0; i < results.length; i++) {
+                        var parkDiv = $("<div>");
+                        var p = $("<p>").html("Rating: " + results[i].rating + "<br>");
+                        // var topicImage = $("<img>");
+                        topicImage.attr({
+                            "src": results[i].images.fixed_height_still.url,
+                            "data-still": results[i].images.fixed_height_still.url,
+                            "data-animate": results[i].images.fixed_height.url,
+                            "data-state": 'still',
+                            "class": 'gif'
+                        });
+                      
+                        
+                        parkDiv.prepend(p);
+                        $("#gifs-appear-here").prepend(parkDiv);
+                    }
+                    
+                });
+                
+        });
+       
+    }
 
 //indiviual park information
 // Directions -google maps api

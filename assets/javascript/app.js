bd13576d81dc0fcef7a7448bb053bd1775e95748
Keith Hemsoth <kevhemsoth@gmail.com>
2019-03-10 10:34:03 -0400

    added app.js script/added user location search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,19 @@
+//  --------------------------------------------------------------------------------
 
+// Obtain user location
+// We may want to use watchPosition() to keep track of user movements
+// Will need button to bind event 
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }   else {
+        console.log("location not available");
+    }
+}
+function showPosition(position) {
+    console.log("latitude: " + position.coords.latitude);
+    console.log("longitude: " + position.coords.longitude);
+}
 
 //  --------------------------------------------------------------------------------
   

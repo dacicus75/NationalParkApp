1e859229253f4d4115d988d2aa6eb5a2a1a84ee9
unknown <kevhemsoth@gmail.com>
2019-03-16 13:04:51 -0400

    sign in and out functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -118,11 +118,23 @@ firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
     if (user) {
         $('[data-target="#sign-in-modal"]').remove();
         $('[data-target="#sign-up-modal"]').remove();
+        $('.navbar-nav').append("<a class='nav-item nav-link' href='#' data-toggle='modal' data-target='#sign-out-modal'>Sign Out</a>")
         console.log("user signed in");
     } else {
         console.log('no user info');
     }
   });
+
+  $('body').on('click', '[data-target="#sign-out-modal"]', function(){
+    firebase.auth().signOut().then(function() {
+        console.log("sign out successful");
+      }).catch(function(error) {
+          console.log(error);
+      });
+      $('[data-target="#sign-out-modal"]').remove();
+      $('.navbar-nav').append('<a class="nav-item nav-link" href="#" data-toggle="modal" data-target="#sign-in-modal">Sign In</a>');
+      $('.navbar-nav').append('<a class="nav-item nav-link" href="#" data-toggle="modal" data-target="#sign-up-modal">Sign Up</a>');
+  })
 
 // User Sign Out
 //firebase.auth().signOut().then(function() {
@@ -351,8 +363,6 @@ $("#your-profile").hide();
 
     //     });
 
-}
-});
 
 // -------------------------------------------------------------------------------------------
 // to delete hike favorite
